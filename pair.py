cd1a619d8900c17a59ce406a117fa78008f8e926
kelleydv <kelleydv@gmail.com>
2016-02-22 11:33:10 -0800

    CLI with filename as a parameter

--- pair.py
+++ pair.py
@@ -1,18 +1,21 @@
-import random
+import random, sys
 
-print("Enter filename:")
-fileName = input()
-f = open(fileName, 'r')
-text = f.read()
-names = text[:-1].split('\n')
-print(len(names))
-print(names)
-if(not len(names)%2 == 0):
-	print("Amount of names not even!")
-else:
-	random.shuffle(names)
+def pair(fileName):
+	f = open(fileName, 'r')
+	text = f.read()
+	names = text[:-1].split('\n')
+	print(len(names))
 	print(names)
-	o = open("pairingOutput.txt", 'w')
-	for i in range(len(names)//2):
-		o.write(names[2*i] + " " + names[2*i + 1] + '\n')
-	o.close()
+	if(not len(names)%2 == 0):
+		print("Amount of names not even!")
+	else:
+		random.shuffle(names)
+		print(names)
+		o = open("%s_groups.txt" % fileName.split('.')[0], 'w')
+		for i in range(len(names)//2):
+			o.write(names[2*i] + " " + names[2*i + 1] + '\n')
+		o.close()
+
+
+if __name__ == "__main__":
+	pair(sys.argv[0])
